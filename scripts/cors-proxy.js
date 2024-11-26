0775a3389720067281009fda12b2f86f67debc2c
Andy Levesque <andy.m.levesque@gmail.com>
2024-11-26 13:43:39 -0600

    Resolve local corsproxy port issue

--- scripts/cors-proxy.js
+++ scripts/cors-proxy.js
@@ -9,6 +9,8 @@ const port = process.env.CORS_PORT || 8888;
 cors_proxy
   .createServer({
     originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
   })
   .listen(port, host, function () {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
